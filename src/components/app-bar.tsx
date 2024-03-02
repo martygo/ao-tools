d5cb4efc6116a90608ee4060ab7a4e5f90f8aa86
Martins Gouveia <gouveiamartins6@gmail.com>
2024-03-02 19:50:54 +0100

    Add favicon.ico and import Image from next/image

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
-import Image from "next/image";
 import { SelectLanguage } from "./select-language";
 import { ThemeModeToggle } from "./theme-mode-toogle";
 
